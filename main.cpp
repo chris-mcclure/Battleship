b624efbaaa3170a498704dccc7fac2abd66d7cec
chris-mcclure <chrismcclure2010@gmail.com>
2018-02-07 16:23:35 -0900

    added comment

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@
 //
 //  Created by Chris McClure on 2/7/18.
 //  Copyright © 2018 Chris McClure & AJ Carvajal. All rights reserved.
-//
+// adding a comment here
 
 #include <stdio.h>
 #include <iostream>
-using std::cout;
+using std::cout; 
 using std::endl;
 int main(void){
     cout << "Hello World!" << endl;
