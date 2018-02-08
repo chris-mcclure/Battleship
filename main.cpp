93da7803c264516a0fbef2e89616c8ecc5fdc010
Chris McClure <chrismcclure2010@gmail.com>
2018-02-07 16:22:37 -0900

    adding main

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Battleship
+//
+//  Created by Chris McClure on 2/7/18.
+//  Copyright © 2018 Chris McClure & AJ Carvajal. All rights reserved.
+//
+
+#include <stdio.h>
+#include <iostream>
+using std::cout;
+using std::endl;
+int main(void){
+    cout << "Hello World!" << endl;
+}
